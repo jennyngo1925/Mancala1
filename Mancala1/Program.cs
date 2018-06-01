c15f4344e10323e66eda5b312a852714ff28fe31
DESKTOP-TJP2DL1\XPS <timngo@yahoo.com>
2018-05-31 21:07:33 -0400

    Initial Commit

---
+++ Mancala1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mancala1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Program = new Program();
+            Program.DrawBoard();
+        }
+
+        void DrawBoard()
+        {
+            Console.WriteLine(@"       ----------------------------------       ");
+            Console.WriteLine(@"     /    |                               \     ");
+            Console.WriteLine(@"   /      |                                 \   ");
+            Console.WriteLine(@"  |                                          |  ");
+            Console.WriteLine(@"  |    ---------------------------------     |  ");
+            Console.WriteLine(@"  |                                          |  ");
+            Console.WriteLine(@"   \                                        /   ");
+            Console.WriteLine(@"     \                                    /     ");
+            Console.WriteLine(@"       ----------------------------------       ");
+            //Console.SetCursorPosition(5, 10);
+            //System.Threading.Thread.Sleep(100);
+            Console.ReadKey();
+        }
+    }
+}
