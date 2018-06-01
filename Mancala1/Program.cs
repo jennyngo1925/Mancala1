89a3232e45151e58d86dbe7cf2967a8f5cb347a5
DESKTOP-TJP2DL1\XPS <timngo@yahoo.com>
2018-05-31 22:28:29 -0400

    Thi finished the blank board

--- Mancala1/Program.cs
+++ Mancala1/Program.cs
@@ -12,21 +12,52 @@ namespace Mancala1
         {
             var Program = new Program();
             Program.DrawBoard();
+            Program.WalkSquare();
         }
 
         void DrawBoard()
         {
-            Console.WriteLine(@"       ----------------------------------       ");
-            Console.WriteLine(@"     /    |                               \     ");
-            Console.WriteLine(@"   /      |                                 \   ");
-            Console.WriteLine(@"  |                                          |  ");
-            Console.WriteLine(@"  |    ---------------------------------     |  ");
-            Console.WriteLine(@"  |                                          |  ");
-            Console.WriteLine(@"   \                                        /   ");
-            Console.WriteLine(@"     \                                    /     ");
-            Console.WriteLine(@"       ----------------------------------       ");
+            Console.WriteLine(@"       -----------------------------------------      ");
+            Console.WriteLine(@"     /   |      |      |      |      |      |    \    ");
+            Console.WriteLine(@"   /     |      |      |      |      |      |     \   ");
+            Console.WriteLine(@"  |      |      |      |      |      |      |      |  ");
+            Console.WriteLine(@"  |      ------------------------------------      |  ");
+            Console.WriteLine(@"  |      |      |      |      |      |      |      |  ");
+            Console.WriteLine(@"   \     |      |      |      |      |      |     /   ");
+            Console.WriteLine(@"     \   |      |      |      |      |      |   /     ");
+            Console.WriteLine(@"       ----------------------------------------       ");
             //Console.SetCursorPosition(5, 10);
             //System.Threading.Thread.Sleep(100);
+            //Console.ReadKey();
+        }
+
+        void WalkSquare()
+        {
+            int n = 1;
+            for (int i = 1; i <= 8; i++)
+            {
+                for (int j = 0; j <= 4; j++)
+                {
+                    Console.SetCursorPosition(j*7+12, 2);
+                    Console.Write(n++);
+                    System.Threading.Thread.Sleep(1000);
+                }
+
+                Console.SetCursorPosition(5 * 7 + 12, 4);
+                Console.Write(n++);
+                System.Threading.Thread.Sleep(1000);
+
+                for (int j = 4; j >=0 ; j--)
+                {
+                    Console.SetCursorPosition(j * 7 + 12, 6);
+                    Console.Write(n++);
+                    System.Threading.Thread.Sleep(1000);
+                }
+
+                Console.SetCursorPosition(-1 * 7 + 12, 4);
+                Console.Write(n++);
+                System.Threading.Thread.Sleep(1000);
+            }
             Console.ReadKey();
         }
     }
