84a9bc815200ddb4bf03120798b502a08c44f8a6
jennyngo1925 <jennyngo1925@gmail.com>
2018-06-24 19:10:08 -0400

    break into parts

---
+++ Mancala1/BoardAction.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mancala1
+{
+    public partial class Mancala
+    {
+        public void DrawBoard()
+        {
+            Console.WriteLine(@"      ------------------------------------------      ");
+            Console.WriteLine(@"    /    |      |      |      |      |      |    \    ");
+            Console.WriteLine(@"   /     |  5   |  5   |  5   |  5   |  5   |     \   ");
+            Console.WriteLine(@"  |      |      |      |      |      |      |      |  ");
+            Console.WriteLine(@"  |  0   ------------------------------------  0   |  ");
+            Console.WriteLine(@"  |      |      |      |      |      |      |      |  ");
+            Console.WriteLine(@"   \     |  5   |  5   |  5   |  5   |  5   |     /   ");
+            Console.WriteLine(@"    \    |      |      |      |      |      |    /    ");
+            Console.WriteLine(@"      ------------------------------------------      ");
+            Console.WriteLine(@"                                                      ");
+            Console.WriteLine(@"Player 1: You                 Player 2: Computer      ");
+            Console.WriteLine(@"Marbles Captured: 0           Marbles Captured: 0     ");
+            Console.WriteLine(@"                                                      ");
+            Console.WriteLine(@"                Current Player: You                   ");
+            Console.WriteLine(@"                Started Square: 0                     ");
+            Console.WriteLine(@"                Current Square: 0                     ");
+            Console.WriteLine(@"                Marbles on Hand: 0                    ");
+        }
+
+        public void DisplayStatus()
+        {
+            if (Player == 1)
+            {
+                Console.SetCursorPosition(32, 13);
+                Console.Write("You           ");
+            }
+            if (Player == 2)
+            {
+                Console.SetCursorPosition(32, 13);
+                Console.Write("Computer");
+            }
+            Console.SetCursorPosition(32, 14);
+            Console.Write(StartSquare);
+            Console.SetCursorPosition(32, 15);
+            Console.Write(CurrentSquare + " ");
+            Console.SetCursorPosition(33, 16);
+        }
+
+
+        public void DisplayMarbles()
+        {
+            int coordX = SquareCoordX[CurrentSquare];
+            int coordY = SquareCoordY[CurrentSquare];
+            Console.SetCursorPosition(coordX, coordY);
+            Console.Write(SquareContent[CurrentSquare]);
+            Console.Write(" ");
+            DisplayStatus();
+        }
+    }
+}
--- Mancala1/Mancala.cs
+++ Mancala1/Mancala.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Mancala1
 {
-    public class Mancala
+    public partial class Mancala
     {
         int[] SquareContent = new int[13] { 0, 5, 5, 5, 5, 5, 0, 5, 5, 5, 5, 5, 0 };
         int[] SquareCoordX = new int[13] { 0, 12, 19, 26, 33, 40, 47, 40, 33, 26, 19, 12, 5 };
@@ -23,27 +23,6 @@ namespace Mancala1
             var Mancala = new Mancala();
             Mancala.DrawBoard();
             Mancala.StartGame();
-        }
-
-        public void DrawBoard()
-        {
-            Console.WriteLine(@"      ------------------------------------------      ");
-            Console.WriteLine(@"    /    |      |      |      |      |      |    \    ");
-            Console.WriteLine(@"   /     |  5   |  5   |  5   |  5   |  5   |     \   ");
-            Console.WriteLine(@"  |      |      |      |      |      |      |      |  ");
-            Console.WriteLine(@"  |  0   ------------------------------------  0   |  ");
-            Console.WriteLine(@"  |      |      |      |      |      |      |      |  ");
-            Console.WriteLine(@"   \     |  5   |  5   |  5   |  5   |  5   |     /   ");
-            Console.WriteLine(@"    \    |      |      |      |      |      |    /    ");
-            Console.WriteLine(@"      ------------------------------------------      ");
-            Console.WriteLine(@"                                                      ");
-            Console.WriteLine(@"Player 1: You                 Player 2: Computer      ");
-            Console.WriteLine(@"Marbles Captured: 0           Marbles Captured: 0     ");
-            Console.WriteLine(@"                                                      ");
-            Console.WriteLine(@"                Current Player: You                   ");
-            Console.WriteLine(@"                Started Square: 0                     ");
-            Console.WriteLine(@"                Current Square: 0                     ");
-            Console.WriteLine(@"                Marbles on Hand: 0                    ");
         }
 
         public void StartGame()
@@ -131,113 +110,6 @@ namespace Mancala1
                 System.Threading.Thread.Sleep(1000);
                 CurrentSquare = AdvanceToNextSquare();
             }
-        }
-
-
-        public void DisplayStatus()
-        {
-            if (Player == 1)
-            {
-                Console.SetCursorPosition(32, 13);
-                Console.Write("You           ");
-            }
-            if (Player == 2)
-            {
-                Console.SetCursorPosition(32, 13);
-                Console.Write("Computer");
-            }
-            Console.SetCursorPosition(32, 14);
-            Console.Write(StartSquare);
-            Console.SetCursorPosition(32, 15);
-            Console.Write(CurrentSquare + " ");
-            Console.SetCursorPosition(33, 16);
-        }
-
-
-        public void DisplayMarbles()
-        {
-            int coordX = SquareCoordX[CurrentSquare];
-            int coordY = SquareCoordY[CurrentSquare];
-            Console.SetCursorPosition(coordX, coordY);
-            Console.Write(SquareContent[CurrentSquare]);
-            Console.Write(" ");
-            DisplayStatus();
-        }
-
-
-        public bool IsCaptureTime()
-        {
-            int currentSquareContent = SquareContent[CurrentSquare];
-
-            int nextSquare = AdvanceToNextSquare();
-            int nextSquareContent = SquareContent[nextSquare];
-
-            if (currentSquareContent == 0 && nextSquareContent > 0)
-                return true;
-            else
-                return false;
-        }
-
-
-        public bool IsStopTime()
-        {
-            int currentSquareContent = SquareContent[CurrentSquare];
-
-            int nextSquare = AdvanceToNextSquare();
-            int nextSquareContent = SquareContent[nextSquare];
-
-            if (currentSquareContent == 0 && nextSquareContent == 0)
-                return true;
-            else
-                return false;
-        }
-
-
-        public bool IsEndTime()
-        {
-            // TODO: End the game if all squares are empty or players connot move annymore
-        }
-
-
-        public int AdvanceToNextSquare()
-        {
-            int nextSquare;
-
-            if (CurrentSquare < 12)
-                nextSquare = CurrentSquare + 1;
-            else
-                nextSquare = 1;
-
-            return nextSquare;
-        }
-
-        /// <summary>
-        /// Chooses the start square for each player
-        /// </summary>
-        /// <returns></returns>
-        public int ChooseStartSquare()
-        {
-            int sq = 0;
-
-            if (Player == 1) // You
-            {
-                while (sq >= 1 && sq <= 5)
-                {
-                    Console.SetCursorPosition(0, 20);
-                    Console.WriteLine("Which box do you choose (1, 2, 3, 4, 5)?");
-                    sq = Convert.ToInt32(Console.ReadLine());
-                }
-            }
-            else if (Player == 2) // Computer
-            {
-                Console.SetCursorPosition(0, 22);
-                Console.Write("Computer's Turn. Press enter to continue.");
-                Console.ReadKey();
-                Random computer = new Random();
-                sq = computer.Next(7, 11);
-            }
-
-            return sq;
         }
     }
 }
