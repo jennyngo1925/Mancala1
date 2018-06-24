573f642cca829f7dfa77c9deb96ec525c92fbe00
jennyngo1925 <jennyngo1925@gmail.com>
2018-06-24 18:48:30 -0400

    ba changed the first while loop

--- Mancala1/Program.cs
+++ Mancala1/Program.cs
@@ -50,97 +50,30 @@ namespace Mancala1
         {
             while (true)
             {
+                //Your turn
                 Player = 1;
                 StartSquare = ChooseStartSquare();
                 CurrentSquare = StartSquare;
                 MarblesOnHand = 0; //Note: Marbles on hand before picking up or dropping down
-                if (MarblesOnHand == 0) //Note: Marbles on hand before picking up or dropping down
+                MoveUntilFinishTurn();
+                if (IsEndTime() == true)
                 {
-                    while (IsCaptureTime() == true && CurrentSquare != 10 && CurrentSquare != 4)
-                    {
-                        CurrentSquare = AdvanceToNextSquare();
-                        MarblesCapturedPlayer += SquareContent[CurrentSquare];
-                        Console.SetCursorPosition(18, 11);
-                        Console.Write(MarblesCapturedPlayer);
-                        SquareContent[CurrentSquare] = 0;
-                        DisplayMarbles();
-                        CurrentSquare = AdvanceToNextSquare();
-                    }
-
-                    MarblesOnHand = SquareContent[CurrentSquare]; //Note: Marbles on hand before picking up or dropping down
-                    SquareContent[CurrentSquare] = 0;
-                }
-                else
-                {
-                    MarblesOnHand--; //Note: Marbles on hand before picking up or dropping down
-                    SquareContent[CurrentSquare] = SquareContent[CurrentSquare] + 1;
+                    break;
                 }
 
-                DisplayMarbles();
-                Console.Write(MarblesOnHand); //Note: Marbles on hand before picking up or dropping down
-                Console.Write(" ");
-                System.Threading.Thread.Sleep(1000);
-                CurrentSquare = AdvanceToNextSquare();
-
-                if (IsStopTime() == true)//computer
+                //Computer's turn
+                Player = 2;
+                StartSquare = ChooseStartSquare();
+                CurrentSquare = StartSquare;
+                MarblesOnHand = 0; //Note: Marbles on hand before picking up or dropping down
+                MoveUntilFinishTurn();
+                if (IsEndTime() == true)
                 {
-                    Player = 2;
-                    Console.SetCursorPosition(0, 22);
-                    Console.Write("Computer's Turn. Press enter to continue.");
-                    Console.ReadKey();
-
-                    MarblesOnHand = 0; //Note: Marbles on hand before picking up or dropping down
-                    Random computer = new Random();
-                    int computerInt = computer.Next(7, 11);
-                    StartSquare = computerInt;
-                    SquareContent[computerInt] = SquareContent[StartSquare];
-
-                    while (SquareContent[StartSquare] == 0)
-                    {
-                        computerInt = computer.Next(7, 11);
-                        StartSquare = computerInt;
-                    }
-
-                    CurrentSquare = StartSquare;
-
-                    while (true)
-                    {
-                        if (MarblesOnHand == 0) //Note: Marbles on hand before picking up or dropping down
-                        {
-                            while (IsCaptureTime() == true && CurrentSquare != 10 && CurrentSquare != 4)
-                            {
-                                CurrentSquare = AdvanceToNextSquare();
-                                int nextSquareContent = SquareContent[CurrentSquare];
-                                int MarblesCaptured = nextSquareContent;
-                                Console.SetCursorPosition(40, 11);
-                                Console.Write(MarblesCapturedComputer);
-                                DisplayMarbles();
-                                SquareContent[CurrentSquare] = 0;
-
-                            }
-                        }
-
-                        if (IsStopTime() == true) //Note: Marbles on hand before picking up or dropping down
-                        {
-                            StartSquare = ChooseStartSquare();
-                            break;//breaks back to the computer
-                        }
-                        else
-                        {
-                            MarblesOnHand--; //Note: Marbles on hand before picking up or dropping down
-                            SquareContent[CurrentSquare] = SquareContent[CurrentSquare] + 1;
-                        }
-                        DisplayMarbles();
-                        Console.Write(MarblesOnHand); //Note: Marbles on hand before picking up or dropping down
-                        Console.Write(" ");
-                        System.Threading.Thread.Sleep(1000);
-
-                        CurrentSquare = AdvanceToNextSquare();
-                        MarblesOnHand = SquareContent[CurrentSquare]; //Note: Marbles on hand before picking up or dropping down
-                        SquareContent[CurrentSquare] = 0;
-                    }
+                    break;
                 }
             }
+
+            // TODO: determine the winner
         }
 
         /// <summary>
@@ -232,6 +165,20 @@ namespace Mancala1
         }
 
 
+        public bool IsCaptureTime()
+        {
+            int currentSquareContent = SquareContent[CurrentSquare];
+
+            int nextSquare = AdvanceToNextSquare();
+            int nextSquareContent = SquareContent[nextSquare];
+
+            if (currentSquareContent == 0 && nextSquareContent > 0)
+                return true;
+            else
+                return false;
+        }
+
+
         public bool IsStopTime()
         {
             int currentSquareContent = SquareContent[CurrentSquare];
@@ -246,17 +193,9 @@ namespace Mancala1
         }
 
 
-        public bool IsCaptureTime()
+        public bool IsEndTime()
         {
-            int currentSquareContent = SquareContent[CurrentSquare];
-
-            int nextSquare = AdvanceToNextSquare();
-            int nextSquareContent = SquareContent[nextSquare];
-
-            if (currentSquareContent == 0 && nextSquareContent > 0)
-                return true;
-            else
-                return false;
+            // TODO: End the game if all squares are empty or players connot move annymore
         }
 
 
