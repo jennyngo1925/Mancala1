1305af3ffe80d9c0b792191de4bfc257670f0b40
thingo1002 <thingo1002@gmail.com>
2018-06-25 20:51:20 -0400

    Added more board status display to help Jenny

--- Mancala1/BoardAction.cs
+++ Mancala1/BoardAction.cs
@@ -33,30 +33,50 @@ namespace Mancala1
             Console.WriteLine(@"Player 1: You                 Player 2: Computer      ");
             Console.WriteLine(@"Marbles Captured: 0           Marbles Captured: 0     ");
             Console.WriteLine(@"                                                      ");
-            Console.WriteLine(@"                Current Player: You                   ");
-            Console.WriteLine(@"                Started Square: 0                     ");
-            Console.WriteLine(@"                Current Square: 0                     ");
+            Console.WriteLine(@"                Current Player:  You                  ");
+            Console.WriteLine(@"                Started Square:  0                   ");
+            Console.WriteLine(@"                Current Square:  0                    ");
             Console.WriteLine(@"                Marbles on Hand: 0                    ");
+            Console.WriteLine(@"                Captured yet?:   No                   ");
+            Console.WriteLine(@"                Lose turn yet?:  No                   ");
+            Console.WriteLine(@"                Game over yet?:  No                   ");
+            Console.WriteLine(@"                                                      ");
+
         }
 
         public void DisplayStatus()
         {
             if (Player == 1)
             {
-                Console.SetCursorPosition(32, 13);
-                Console.Write("You           ");
+                Console.SetCursorPosition(33, 13);
+                Console.Write("You          ");
             }
             if (Player == 2)
             {
-                Console.SetCursorPosition(32, 13);
+                Console.SetCursorPosition(33, 13);
                 Console.Write("Computer");
             }
-            Console.SetCursorPosition(32, 14);
-            Console.Write(StartSquare);
-            Console.SetCursorPosition(32, 15);
-            Console.Write(CurrentSquare + " ");
+            Console.SetCursorPosition(33, 14); 
+            Console.Write(StartSquare + " ");
+            Console.SetCursorPosition(33, 15);
+            Console.Write(CurrentSquare + "  ");
             Console.SetCursorPosition(33, 16);
-            Console.Write(MarblesOnHand + " "); //Note: Marbles on hand before picking up or dropping down
+            Console.Write(MarblesOnHand + " "); //Note: Marbles on hand before picking up or dropping down 
+            Console.SetCursorPosition(33, 17);
+            if (IsCaptureTime() == true)
+                Console.Write("Yes  ");
+            else
+                Console.Write("No   ");
+            Console.SetCursorPosition(33, 18);
+            if (IsTurnEnd() == true)
+                Console.Write("Yes ");
+            else
+                Console.Write("No ");
+            Console.SetCursorPosition(33, 19);
+            if (IsGameEnd() == true)
+                Console.Write("Yes ");
+            else
+                Console.Write("No ");
         }
 
 
@@ -132,7 +152,7 @@ namespace Mancala1
             {
                 while (sq < 1 || sq > 5)
                 {
-                    Console.SetCursorPosition(0, 20);
+                    Console.SetCursorPosition(0, 22);
                     Console.WriteLine("Which box do you choose (1, 2, 3, 4, 5)?");
                     sq = Convert.ToInt32(Console.ReadLine());
                 }
